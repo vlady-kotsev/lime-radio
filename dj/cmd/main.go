d45de939e3fd9dca588691b1f3ff4379433826bc
vlady-kotsev <vladislav.kotsev@limechain.tech>
2025-12-26 20:27:53 +0200

    fix: go version, dep injection

--- dj/cmd/main.go
+++ dj/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/vlady-kotsev/lime-radio/dj/internal/handler"
 	"github.com/vlady-kotsev/lime-radio/dj/internal/server"
 	"github.com/vlady-kotsev/lime-radio/dj/internal/service/payment"
+	"github.com/vlady-kotsev/lime-radio/dj/internal/service/transaction"
 	sharedconfig "github.com/vlady-kotsev/lime-radio/shared/config"
 	sharedserver "github.com/vlady-kotsev/lime-radio/shared/server"
 
@@ -27,6 +28,7 @@ func main() {
 			fx.Annotate(config.Load, fx.As(new(sharedconfig.AuthConfiger)), fx.As(new(sharedconfig.Configer)), fx.As(new(config.PaymentConfiger))),
 			fx.Annotate(auth.NewJWTService, fx.As(new(auth.JWTServicer))),
 			fx.Annotate(payment.NewPaymentService, fx.As(new(payment.PaymentServicer))),
+			fx.Annotate(transaction.NewTransactionService, fx.As(new(transaction.TransactionServicer))),
 			fx.Annotate(client.NewSolanaClient, fx.As(new(client.SolanaClienter))),
 		),
 		handler.ProvideHandlers(),
