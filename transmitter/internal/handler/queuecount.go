7f72857f0369779c6fd00c6f6c50b06407ddd3d5
vlady-kotsev <vladislav.kotsev@limechain.tech>
2026-01-02 18:59:45 +0200

    feat: augment queue-count viewmodel

--- transmitter/internal/handler/queuecount.go
+++ transmitter/internal/handler/queuecount.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/vlady-kotsev/lime-radio/shared/handler"
+	"github.com/vlady-kotsev/lime-radio/transmitter/internal/config"
 	songviewmodel "github.com/vlady-kotsev/lime-radio/transmitter/internal/handler/viewmodel"
 	"github.com/vlady-kotsev/lime-radio/transmitter/internal/service/radio"
 	"go.uber.org/zap"
@@ -10,24 +11,26 @@ import (
 
 type GetQueueCountHandler struct {
 	pl     radio.PlaylistServicer
+	config config.RadioConfiger
 	logger *zap.Logger
 	path   string
 }
 
 var _ handler.Handlerer = (*GetQueueCountHandler)(nil)
 
-func NewQueueCountHandler(pl radio.PlaylistServicer, logger *zap.Logger) *GetQueueCountHandler {
+func NewQueueCountHandler(pl radio.PlaylistServicer, logger *zap.Logger, config config.RadioConfiger) *GetQueueCountHandler {
 	return &GetQueueCountHandler{
 		pl:     pl,
 		logger: logger,
+		config: config,
 		path:   "/queue-count",
 	}
 }
 
 func (h *GetQueueCountHandler) Handle(c *fiber.Ctx) error {
 	songs := h.pl.GetAllSongsInQueue()
-
-	return c.JSON(songviewmodel.NewQueueCountViewModel(len(songs)))
+	maxQueueCount := h.config.GetQueueMaxCount()
+	return c.JSON(songviewmodel.NewQueueCountViewModel(len(songs), maxQueueCount))
 }
 
 func (h *GetQueueCountHandler) RegisterRoute(app *fiber.App) {
--- transmitter/internal/handler/viewmodel/queue.go
+++ transmitter/internal/handler/viewmodel/queue.go
@@ -1,9 +1,10 @@
 package viewmodel
 
 type QueueCountViewModel struct {
-	Count int `json:"queue_count"`
+	Count    int `json:"queue_count"`
+	MaxCount int `json:"max_queue_count"`
 }
 
-func NewQueueCountViewModel(count int) *QueueCountViewModel {
-	return &QueueCountViewModel{Count: count}
+func NewQueueCountViewModel(count, maxCount int) *QueueCountViewModel {
+	return &QueueCountViewModel{Count: count, MaxCount: maxCount}
 }
